95f1757ab45e8f11c50c12fc67b7d93396fffc51
Mahima Thacker <81761152+Thackermahima@users.noreply.github.com>
2022-03-23 18:11:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import { Container } from '@mui/material';
-// import Navbar from './components/Navbar';
-// import Intro from './components/Intro';
-// import Bugs from './components/Bugs';
-// import Compaigns from './components/Compaigns';
-// import QandAList from './components/QandAList';
-// import BountyWinners from './components/BountyWinners';
-// import Footer from './components/Footer';
+import { Container } from '@mui/material';
+import Navbar from './components/Navbar';
+import Intro from './components/Intro';
+import Bugs from './components/Bugs';
+import Compaigns from './components/Compaigns';
+import QandAList from './components/QandAList';
+import BountyWinners from './components/BountyWinners';
+import Footer from './components/Footer';
 // import DetailBounties from './components/DetailBounties';
 // import DetailQandA from './components/DetailQandA';
 // import {
@@ -15,14 +15,14 @@ import React from 'react'
 //   Route,
 //   Link
 // }from "react-router-dom";
-import DashBoard from './Dashboard'
+// import DashBoard from './Dashboard'
 
 const App = () => {
   return (
     <>
 {/* <Router> */}
 
-{/* <Navbar />
+<Navbar />
 
 <Container>
 <Intro />
@@ -31,8 +31,22 @@ const App = () => {
 <QandAList />
 <BountyWinners />
 </Container>
-<Footer /> */}
-<DashBoard />
+<Footer />
+{/* <DashBoard /> */}
+
+  {/* <Routes>
+          {/* <Route path="/Q&A" element={<QandAList />}/>  */}
+
+          {/* <Route path="/bounty" element={<DetailBounties />} />
+        </Routes> 
+         </Router> } */}
+
+ </>
+
+    )
+}
+
+export default App
 
   {/* <Routes>
           {/* <Route path="/Q&A" element={<QandAList />}/>  */}
