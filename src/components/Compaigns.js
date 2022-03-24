796eb8450ad46f3c957a5959156810ac171c3168
Mahima Thacker <81761152+Thackermahima@users.noreply.github.com>
2022-03-24 11:59:33 +0530

    Update Compaigns.js

--- src/components/Compaigns.js
+++ src/components/Compaigns.js
@@ -32,9 +32,9 @@ view all
 <Box
 
 sx={{
-    height: '216px',
-    width: '416px',
-    border: '2px solid #545353',
+    height: '193px',
+    width: '473px',
+    border: '1px solid #D82148',
     background: '#FFFFFF',
     p: 1,
     m:1,
@@ -44,55 +44,36 @@ sx={{
         <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}> Prize :<span style={{color:"#D82148" , fontWeight:"10"}}>  1000 USDC and polygon Dev NFT </span></Typography>
         <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Category :<span style={{ fontWeight:"10"}}>  Security | Functional | UI/UX </span></Typography>
         <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Duration : <span style={{ fontWeight:"10"}}> 10 March 2022 to 22 April 2022 </span></Typography>
-<Button  size="medium" style={{fontSize:"11px", fontWeight:"bold", backgroundColor:"#D82148" ,color:'white', marginTop:'26px',textTransform:'capitalize'}}>Paticipate</Button>
+<Button  size="medium" style={{fontSize:"11px", fontWeight:"bold", backgroundColor:"#D82148" ,color:'white', marginTop:'18px',textTransform:'capitalize'}}>Paticipate</Button>
 
 </Box>
 <Box
 
 sx={{
-    height: '216px',
-    width: '416px',
-    border: '2px solid #545353',
+    height: '193px',
+    width: '473px',
+    border: '1px solid #D82148',
     background: '#FFFFFF',
     p: 1,
     m: 1,
-    ml:25
+    ml:10
 }}
 >
 <Typography variant="subtitle1" fontStyle='bold' fontWeight= "700" ml={1} mb={1} mt={1}>Security Bounty report Challange by Polygon</Typography>
         <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}> Prize :<span style={{color:"#D82148" , fontWeight:"10"}}>  1000 USDC and polygon Dev NFT </span></Typography>
         <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Category :<span style={{ fontWeight:"10"}}>  Security | Functional | UI/UX </span></Typography>
         <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Duration : <span style={{ fontWeight:"10"}}> 10 March 2022 to 22 April 2022 </span></Typography>
-        <Button  size="medium" style={{fontSize:"11px", fontWeight:"bold", backgroundColor:"#D82148" ,color:'white', marginTop:'26px',textTransform:'capitalize'}}>Paticipate</Button>
+        <Button  size="medium" style={{fontSize:"11px", fontWeight:"bold", backgroundColor:"#D82148" ,color:'white', marginTop:'18px',textTransform:'capitalize'}}>Paticipate</Button>
 </Box>
 <Box
 
 sx={{
-    height: '216px',
-    width: '416px',
-    border: '2px solid #545353',
+    height: '193px',
+    width: '473px',
+    border: '1px solid #D82148',
     background: '#FFFFFF',
     p: 1,
     m: 1,
-
-}}
->
-<Typography variant="subtitle1" fontStyle='bold' fontWeight= "700" ml={1} mb={1} mt={1}>Security Bounty report Challange by Polygon</Typography>
-        <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}> Prize :<span style={{color:"#D82148" , fontWeight:"10"}}>  1000 USDC and polygon Dev NFT </span></Typography>
-        <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Category :<span style={{ fontWeight:"10"}}>  Security | Functional | UI/UX </span></Typography>
-        <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Duration : <span style={{ fontWeight:"10"}}> 10 March 2022 to 22 April 2022 </span></Typography>
-        <Button  size="medium" style={{fontSize:"11px", fontWeight:"bold", backgroundColor:"#D82148" ,color:'white', marginTop:'26px',textTransform:'capitalize'}}>Paticipate</Button>
-</Box>
-<Box
-
-sx={{
-    height: '216px',
-    width: '416px',
-    border: '2px solid #545353',
-    background: '#FFFFFF',
-    p: 1,
-    m: 1,
-    ml:25
 
 }}
 >
@@ -100,7 +81,26 @@ sx={{
         <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}> Prize :<span style={{color:"#D82148" , fontWeight:"10"}}>  1000 USDC and polygon Dev NFT </span></Typography>
         <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Category :<span style={{ fontWeight:"10"}}>  Security | Functional | UI/UX </span></Typography>
         <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Duration : <span style={{ fontWeight:"10"}}> 10 March 2022 to 22 April 2022 </span></Typography>
-        <Button  size="medium" style={{fontSize:"11px", fontWeight:"bold", backgroundColor:"#D82148" ,color:'white', marginTop:'26px',textTransform:'capitalize'}}>Paticipate</Button>
+        <Button  size="medium" style={{fontSize:"11px", fontWeight:"bold", backgroundColor:"#D82148" ,color:'white', marginTop:'18px',textTransform:'capitalize'}}>Paticipate</Button>
+</Box>
+<Box
+
+sx={{
+    height: '193px',
+    width: '473px',
+    border: '1px solid #D82148',
+    background: '#FFFFFF',
+    p: 1,
+    m: 1,
+    ml:10
+
+}}
+>
+<Typography variant="subtitle1" fontStyle='bold' fontWeight= "700" ml={1} mb={1} mt={1}>Security Bounty report Challange by Polygon</Typography>
+        <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}> Prize :<span style={{color:"#D82148" , fontWeight:"10"}}>  1000 USDC and polygon Dev NFT </span></Typography>
+        <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Category :<span style={{ fontWeight:"10"}}>  Security | Functional | UI/UX </span></Typography>
+        <Typography variant="subtitle2" fontStyle='bold' fontWeight= "550" ml={1} mt={1}>Duration : <span style={{ fontWeight:"10"}}> 10 March 2022 to 22 April 2022 </span></Typography>
+        <Button  size="medium" style={{fontSize:"11px", fontWeight:"bold", backgroundColor:"#D82148" ,color:'white', marginTop:'18px',textTransform:'capitalize'}}>Paticipate</Button>
 </Box>
 </Stack>
 </Container>
@@ -111,4 +111,5 @@ sx={{
 }
 
 export default Compaigns
+
 
